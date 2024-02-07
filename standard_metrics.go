40537e2df4c609603a9d56a0bdf863d8b62e8461
James Trotter <james.trotter@tempmee.com>
2024-02-07 11:14:00 -0500

    fix: standard metric impl

--- standard_metrics.go
+++ standard_metrics.go
@@ -2,6 +2,7 @@ package metrics_lib
 
 import (
 	"net/http"
+	"time"
 )
 
 const (
@@ -13,6 +14,7 @@ type StandardMetrics interface {
 	ResolverMetric(value float64, labels ResolverMetricLabels) error
 	HttpMiddlewareMetric(config HttpMiddlewareMetricConfig) func(http.Handler) http.Handler
 	ApiMetric(value float64, labels ApiMetricLabels) error
+	ApiMetricDuration(startTime time.Time, labels ApiMetricLabels, err error) error
 	DatabaseMetric(value float64, labels DatabaseMetricLabels) error
 	CallMetric(value float64, labels CallMetricLabels) error
 }
